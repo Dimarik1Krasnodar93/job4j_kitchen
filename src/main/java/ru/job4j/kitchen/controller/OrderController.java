60066d0188317148393ffb7dc7bd64d672cdd8ec
Dmitry <krasnodar93@mail.ru>
2023-04-30 12:59:55 +0300

    first commit

---
+++ src/main/java/ru/job4j/kitchen/controller/OrderController.java
@@ -0,0 +1,18 @@
+package ru.job4j.kitchen.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import ru.job4j.kitchen.domain.Order;
+import ru.job4j.kitchen.service.KitchenService;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+    KitchenService kitchenService;
+    @PostMapping("/")
+    public void create(@RequestBody Order order) {
+        kitchenService.receiveOrder(order);
+    }
+
+}
